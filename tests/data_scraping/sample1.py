5d5ea58147ee405cd7212ec1a11f289366bd80fb
stundet-ops <gogakuec@mail.com>
2023-07-14 14:19:22 +0900

    update for map

--- tests/data_scraping/sample1.py
+++ tests/data_scraping/sample1.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 from urllib.parse import urljoin
+from urllib.parse import urlparse
 # Set the base URL
 base_url = "https://sun.ac.jp/"
 # Set the maximum depth for crawling
@@ -59,11 +60,21 @@ def crawl_single_url(url):
             print(f"Error occurred while crawling URL: {url}")
             print(f"Error details: {e}\n")
     
-    with open(file_path+"/sample.txt", "a") as f:
+    parsed_url = urlparse(url)
+    file_name = parsed_url.path.strip("/").replace("/", "_") + ".txt"
+
+    with open(f"{file_path}/{file_name}", "a") as f:
         f.write(file_contents)
+
     
 # Start crawling from the base URL
 file_path = "./data/faculty"
 # crawl_url(base_url, 0)
+# crawl_single_url("https://sun.ac.jp/department/systems/systems/")
+url_list = [
+    "https://sun.ac.jp/department/systems/systems/",
+    "https://sun.ac.jp/department/systems/systems/",
+]
 
-crawl_single_url("https://sun.ac.jp/department/systems/systems/")
+for url in url_list:
+    crawl_single_url(url)
